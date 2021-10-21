771e5402b18ba641805bb5c079877e53c2a001e2
Khoa <khoadh28@gmail.com>
2021-10-21 19:56:48 +0700

    project created

---
+++ src/main/java/com/betvn/aptech88/Aptech88Application.java
@@ -0,0 +1,13 @@
+package com.betvn.aptech88;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Aptech88Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Aptech88Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/betvn/aptech88/Aptech88ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.betvn.aptech88;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Aptech88ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
